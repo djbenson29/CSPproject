9c8d983a8b7a46885c0ea134a5dec5faaba31a1f
djbenson29 <djbenson@wpi.edu>
2015-02-17 17:31:31 -0500

    Adds the add item method to the bags class

--- CSPproject/src/Bag.java
+++ CSPproject/src/Bag.java
@@ -2,13 +2,22 @@
 public class Bag {
 	
 	int weight;
-	String[] listOfItems;
+	String[] listOfItems = new String[100];
+	int numItems;
 	
 	public Bag(int weight, String[] listOfItems){
 		this.weight = weight;
 		this.listOfItems = listOfItems;
+		this.numItems = 0;
 	}
 	
-
+	// Adds an item to the bag
+	public void addItem(String item, int itemWeight){
+		this.listOfItems[this.numItems] = item;
+		this.weight += itemWeight;
+		this.numItems++;
+	}
+	
+	
 }
 
